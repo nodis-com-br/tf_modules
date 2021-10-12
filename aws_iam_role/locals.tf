52c5c3f1bbb0e919399788d7b727414cf7f2dd7b
ptonini <pedro.tonini@nodis.com.br>
2021-10-12 18:04:07 -0300

    updated aws_iam_roles

--- aws_iam_role/locals.tf
+++ aws_iam_role/locals.tf
@@ -1,7 +1,7 @@
 locals {
   builtin_policies = {
-    admin = 'arn:aws:iam::aws:policy/AdministratorAccess'
-    ec2_admin = 'arn:aws:iam::aws:policy/AmazonEC2FullAccess'
+    admin = "arn:aws:iam::aws:policy/AdministratorAccess"
+    ec2_admin = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
   }
   selected_builtin_policy_arns = {for l in var.builtin_policies : l => local.builtin_policies[l]}
   policy_arns = concat(var.policy_arns, local.selected_builtin_policy_arns)
