865a5bd28bdff6ae8115112249a3d5ecf60bc873
ptonini <pedro.tonini@nodis.com.br>
2021-10-13 00:33:54 -0300

    updated azure_aks module

--- azure_aks/locals.tf
+++ azure_aks/locals.tf
@@ -3,11 +3,12 @@ locals {
   vault_kv_secrets_path = "secret/k8s/${var.rg.name}"
   default_node_pool_min_count = var.default_node_pool_min_count == null ? var.default_node_pool_node_count : var.default_node_pool_min_count
   default_node_pool_max_count = var.default_node_pool_max_count == null ? var.default_node_pool_node_count : var.default_node_pool_max_count
+  ip_endpont = "https://${data.dns_a_record_set.endpoint_ip.addrs.0}:443"
   kubeconfig = jsonencode({
     raw = azurerm_kubernetes_cluster.this.kube_config_raw
     kubedict = jsonencode({
       cluster = {
-        server = "https://${data.dns_a_record_set.endpoint_ip.addrs.0}:443"
+        server = azurerm_kubernetes_cluster.this.kube_config.0.host
         certificate-authority-data = azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate
       }
       user = {
