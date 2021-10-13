769b004ef27d97e0e2e1862958e54cffff93799b
ptonini <pedro.tonini@nodis.com.br>
2021-10-13 01:16:42 -0300

    updated azure_aks module

--- azure_aks/data.tf
+++ azure_aks/data.tf
@@ -27,5 +27,5 @@ data "kubernetes_secret" "vault-injector-token" {
 }
 
 data "dns_a_record_set" "endpoint_ip" {
-  host = trimsufix(trimprefix(azurerm_kubernetes_cluster.this.kube_config.0.host, "https://"), ":443")
+  host = trimprefix(azurerm_kubernetes_cluster.this.kube_config.0.host, "https://")
 }
