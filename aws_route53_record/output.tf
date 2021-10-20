b526ae25b2c623adcfddaa388dc3e69ce7fdf300
ptonini <pedro.tonini@nodis.com.br>
2021-10-20 17:20:54 -0300

    updated route53 module

--- aws_route53_record/output.tf
+++ aws_route53_record/output.tf
@@ -1,3 +1,7 @@
+output "this" {
+  value = var.alias == null ? aws_route53_record.this : aws_route53_record.alias
+}
+
 output "certificate" {
   value = var.create_certificate ? aws_acm_certificate.this.0 : null
 }
