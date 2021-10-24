df9988739e9615a0a0d35331a39b203bad909b9f
ptonini <pedro.tonini@nodis.com.br>
2021-10-24 15:24:18 -0300

    refactoring

--- vault_k8s_auth/main.tf
+++ vault_k8s_auth/main.tf
@@ -1,10 +1,11 @@
-resource "vault_auth_backend" "this" {
+module "auth_backend" {
+  source = "../vault_auth_backend"
   type = "kubernetes"
   path = var.path
 }
 
 resource "vault_kubernetes_auth_backend_config" "this" {
-  backend = vault_auth_backend.this.path
+  backend = module.auth_backend.this.path
   kubernetes_host = var.host
   kubernetes_ca_cert = var.ca_certificate
   token_reviewer_jwt = var.token
