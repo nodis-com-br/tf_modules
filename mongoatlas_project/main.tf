634534a8ca89dddb6a560cc1a938ce634d257295
ptonini <pedro.tonini@nodis.com.br>
2022-04-06 17:34:16 -0300

    updating mongoatlas_project module

--- mongoatlas_project/main.tf
+++ mongoatlas_project/main.tf
@@ -10,6 +10,26 @@ resource "mongodbatlas_project_ip_access_list" "this" {
   comment = each.key
 }
 
+resource "mongodbatlas_network_container" "this" {
+  count = var.peering == null ? 0 : 1
+  project_id = mongodbatlas_project.this.id
+  atlas_cidr_block = try(var.peering.atlas_cidr_block, null)
+  provider_name = var.provider_name
+  region = var.provider_region_name
+}
+
+resource "mongodbatlas_network_peering" "this" {
+  count = var.peering == null ? 0 : 1
+  project_id = mongodbatlas_project.this.id
+  container_id = mongodbatlas_network_container.this.0.container_id
+  provider_name = var.provider_name
+  azure_directory_id = try(var.peering.azure_directory_id, null)
+  azure_subscription_id = try(var.peering.azure_subscription_id, null)
+  resource_group_name = try(var.peering.resource_group_name, null)
+  vnet_name = try(var.peering.vnet_name, null)
+}
+
+
 resource "vault_database_secret_backend_connection" "this" {
   count = var.vault_secret_backend_config_name == null ? 0 : 1
   backend = var.vault_path
--- mongoatlas_project/variables.tf
+++ mongoatlas_project/variables.tf
@@ -14,8 +14,22 @@ variable "root_rotation_statements" {
   default = []
 }
 
+
+variable "provider_name" {
+  default = "AZURE"
+}
+
+variable "provider_region_name" {
+  default = "US_EAST"
+}
+
+
 variable "vault_path" {
   default = "mongodb"
+}
+
+variable "vault_admin_role_default_ttl" {
+  default = 604800
 }
 
 variable "vault_secret_backend_credentials" {
@@ -26,6 +40,10 @@ variable "vault_secret_backend_config_name" {
   default = null
 }
 
-variable "vault_admin_role_default_ttl" {
-  default = 604800
+variable "atlas_cidr_block" {
+  default = null
+}
+
+variable "peering" {
+  default = null
 }
