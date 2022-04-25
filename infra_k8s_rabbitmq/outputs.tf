071b176bb380c8197fe3220397c1ffb4db80e1b9
ptonini <pedro.tonini@nodis.com.br>
2022-04-24 23:37:21 -0300

    edited infra_k8s_rabbitmq modules

--- infra_k8s_rabbitmq/outputs.tf
+++ infra_k8s_rabbitmq/outputs.tf
@@ -1,7 +1,7 @@
-output "credentials" {
-  value = {
-    uri = "${var.management_schema}://${var.name}.${var.subdomain}:${var.management_port}"
-    username = data.kubernetes_secret.default_user[0].data.username
-    password = data.kubernetes_secret.default_user[0].data.password
-  }
-}
+#output "credentials" {
+#  value = {
+#    uri = "${var.management_schema}://${var.name}.${var.subdomain}:${var.management_port}"
+#    username = data.kubernetes_secret.default_user[0].data.username
+#    password = data.kubernetes_secret.default_user[0].data.password
+#  }
+#}
