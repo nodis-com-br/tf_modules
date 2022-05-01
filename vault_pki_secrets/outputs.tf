02a1416beae6e961fd5f1a53d8b5ea8a25b9cf54
ptonini <pedro.tonini@nodis.com.br>
2022-05-01 03:25:56 -0300

    updated kubectl_manifest module

--- vault_pki_secrets/outputs.tf
+++ vault_pki_secrets/outputs.tf
@@ -5,3 +5,7 @@ output "this" {
 output "csr" {
   value = vault_pki_secret_backend_intermediate_cert_request.this.csr
 }
+
+output "certificate" {
+  value = vault_pki_secret_backend_intermediate_set_signed.this.certificate
+}
