9cc890f3008843e1cba94813a49f288a555cd19f
ptonini <pedro.tonini@nodis.com.br>
2022-04-30 23:36:52 -0300

    updated kubectl_manifest module

--- kubectl_manifests/locals.tf
+++ kubectl_manifests/locals.tf
@@ -1,19 +1,19 @@
 locals {
-  http_manifests = flatten([
+  http_manifests = var.type == "url" ? flatten([
     for s in data.kubectl_file_documents.http : [
       for k, v in s.manifests : {
         id = k
         yaml = v
       }
     ]
-  ])
-  file_manifests = flatten([
+  ]) : []
+  file_manifests = var.type == "file" ? flatten([
     for s in data.kubectl_file_documents.file : [
       for k, v in s.manifests : {
         id = k
         yaml = v
       }
     ]
-  ])
+  ]) : []
   manifests = concat(local.http_manifests, local.file_manifests)
 }
