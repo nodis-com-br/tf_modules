451d92cd43b39d3118d0ea2cbac04540a3428ac7
ptonini <pedro.tonini@nodis.com.br>
2022-05-07 11:30:05 -0300

    refactored module outputs

--- aws_iam_user/outputs.tf
+++ aws_iam_user/outputs.tf
@@ -6,8 +6,18 @@ output "arn" {
   value = aws_iam_user.this.arn
 }
 
-output "access_key" {
-  value = var.access_key ? aws_iam_access_key.this.0 : null
+output "access_key_id" {
+  value = try(aws_iam_access_key.this[0].id, null)
+  sensitive = true
+}
+
+output "secret_access_key" {
+  value = try(aws_iam_access_key.this[0].secret, null)
+  sensitive = true
+}
+
+output "ses_secret_access_key" {
+  value = try(aws_iam_access_key.this[0].ses_smtp_password_v4, null)
   sensitive = true
 }
 
