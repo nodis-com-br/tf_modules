c510ca046244635fff5276489f3b1a46eb86cb61
ptonini <pedro.tonini@nodis.com.br>
2022-05-10 00:31:56 -0300

    refactoring

--- aws_iam_policies/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_iam_policy" "this" {
-  provider = aws.current
-  for_each = toset(local.policy_files)
-  name = each.key
-  policy = data.local_file.policy[each.key].content
-}
--- aws_iam_policies/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "policies" {
-  value = aws_iam_policy.this
-}
