03b801e4e68f53d3df1d405fdfa801273e47f761
ptonini <pedro.tonini@nodis.com.br>
2022-05-17 19:28:01 -0300

    updated azure_aks module output

--- azure_aks/locals.tf
+++ azure_aks/locals.tf
@@ -9,5 +9,7 @@ locals {
     client_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
     client_key = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+    username = azurerm_kubernetes_cluster.this.kube_config.0.username
+    password = azurerm_kubernetes_cluster.this.kube_config.0.password
   }
 }
